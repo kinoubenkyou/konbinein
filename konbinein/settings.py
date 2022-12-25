954e06e6a84c3f8301cf35b98577bfdb4f01eb07
kinoubenkyou <kinoubenkyou@gmail.com>
2022-12-25 17:02:59 +0700

    Add postgres docker service

--- konbinein/settings.py
+++ konbinein/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "konbinein.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "127.0.0.1",
+        "NAME": "postgres",
+        "PASSWORD": "example",
+        "USER": "postgres",
     }
 }
 
