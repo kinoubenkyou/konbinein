5970b0b6240c0eccf1d92e1e5edc75a30ece5c9e
kinoubenkyou <kinoubenkyou@gmail.com>
2023-01-02 11:35:35 +0700

    Add APPEND_SLASH setting

--- konbinein/settings.py
+++ konbinein/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # 'django.middleware.security.SecurityMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
+    "django.middleware.common.CommonMiddleware",
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
@@ -157,3 +157,6 @@ LOGGING = {
 REST_FRAMEWORK = {
     "UNAUTHENTICATED_USER": None,
 }
+
+
+APPEND_SLASH = True
