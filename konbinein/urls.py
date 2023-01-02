13a74889ec2c0d5468e0543e1ca32a6d51c2b71d
kinoubenkyou <kinoubenkyou@gmail.com>
2023-01-02 11:54:28 +0700

    Edit APPEND_SLASH setting

--- konbinein/urls.py
+++ konbinein/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 
 from main.views import OrderViewSet
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 router.register("orders", OrderViewSet)
 urlpatterns = router.urls
