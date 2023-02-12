c208fdeed100b06beadb7fed96d3157997a7e096
kinoubenkyou <kinoubenkyou@gmail.com>
2023-02-12 20:51:32 +0700

    Revert "#53 Add UserPermission"
    This reverts commit e69c5a6a5ad203ee1051fc6bdc0ea71acd96d155.

--- main/permissions.py
+++ main/permissions.py
@@ -4,8 +4,3 @@ from rest_framework.permissions import BasePermission
 class AuthenticatedPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user is not None
-
-
-class UserPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user
