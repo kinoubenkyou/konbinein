c6e54fa1740b36129847757953c7fb33e34a965e
kinoubenkyou <kinoubenkyou@gmail.com>
2023-03-02 16:36:06 +0700

    #62 Add organization authorization for order use case

--- main/tests/views/test_order_view.py
+++ main/tests/views/test_order_view.py
@@ -4,15 +4,22 @@ from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from main.factories.order_factory import OrderFactory
 from main.factories.order_item_factory import OrderItemFactory
 from main.factories.organization_factory import OrganizationFactory
+from main.factories.personnel_factory import PersonnelFactory
 from main.tests.views import TokenAuthenticatedTestCase
 
 
 class OrderViewSetTestCase(TokenAuthenticatedTestCase):
     def test_create(self):
+        organization = OrganizationFactory.create()
+        PersonnelFactory.create(
+            does_organization_agree=True,
+            does_user_agree=True,
+            organization=organization,
+            user=self.user,
+        )
         built_order = OrderFactory.build()
         built_order_items = OrderItemFactory.build_batch(2)
-        organization = OrganizationFactory.create()
-        path = reverse("order-list")
+        path = reverse("order-list", kwargs={"organization_id": organization.id})
         data = {
             "code": built_order.code,
             "created_at": built_order.created_at,
@@ -24,18 +31,25 @@ class OrderViewSetTestCase(TokenAuthenticatedTestCase):
                 }
                 for order_item in built_order_items
             ),
-            "organization": organization.id,
         }
         response = self.client.post(path, data, format="json")
         self.assertEqual(response.status_code, HTTP_201_CREATED)
 
     def test_partial_update(self):
-        order = OrderFactory.create()
+        organization = OrganizationFactory.create()
+        PersonnelFactory.create(
+            does_organization_agree=True,
+            does_user_agree=True,
+            organization=organization,
+            user=self.user,
+        )
+        order = OrderFactory.create(organization_id=organization.id)
         built_order = OrderFactory.build()
         built_order_items = OrderItemFactory.build_batch(2)
         order_items = OrderItemFactory.create_batch(2, order=order)
-        organization = OrganizationFactory.create()
-        path = reverse("order-detail", kwargs={"pk": order.id})
+        path = reverse(
+            "order-detail", kwargs={"organization_id": organization.id, "pk": order.id}
+        )
         data = {
             "code": built_order.code,
             "created_at": built_order.created_at,
@@ -52,7 +66,6 @@ class OrderViewSetTestCase(TokenAuthenticatedTestCase):
                     "unit_price": built_order_items[1].unit_price,
                 },
             ),
-            "organization": organization.id,
         }
         response = self.client.patch(path, data, format="json")
         self.assertEqual(response.status_code, HTTP_200_OK)
--- main/views/order_view.py
+++ main/views/order_view.py
@@ -3,12 +3,13 @@ from rest_framework.viewsets import ModelViewSet
 
 from main.models.order import Order
 from main.models.order_item import OrderItem
+from main.permissions.organization_permission import OrganizationPermission
 from main.permissions.user_permission import UserPermission
 from main.serializers.order_serializer import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = (UserPermission,)
+    permission_classes = (OrganizationPermission, UserPermission)
     queryset = (
         Order.objects.prefetch_related(
             Prefetch(
@@ -22,3 +23,8 @@ class OrderViewSet(ModelViewSet):
         .all()
     )
     serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(organization=self.kwargs["organization_id"])
+        )
