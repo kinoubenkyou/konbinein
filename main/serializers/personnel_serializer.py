43fd6d9696ae208e862c693a8ffdb5e5545c09df
kinoubenkyou <kinoubenkyou@gmail.com>
2023-03-22 16:00:25 +0700

    #76 Change to unique_together

--- main/serializers/personnel_serializer.py
+++ main/serializers/personnel_serializer.py
@@ -1,4 +1,3 @@
-from django.db import transaction
 from rest_framework.serializers import ModelSerializer
 
 from main.models.personnel import Personnel
@@ -6,20 +5,5 @@ from main.models.personnel import Personnel
 
 class PersonnelSerializer(ModelSerializer):
     class Meta:
-        fields = (
-            "does_organization_agree",
-            "does_user_agree",
-            "id",
-            "organization",
-        )
+        fields = "__all__"
         model = Personnel
-        read_only_fields = ("does_organization_agree", "does_user_agree")
-
-    @transaction.atomic
-    def create(self, validated_data):
-        personnel_attributes = validated_data | {
-            "does_organization_agree": False,
-            "does_user_agree": True,
-            "user_id": self.context["request"].user.id,
-        }
-        return super().create(personnel_attributes)
