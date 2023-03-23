bfd91c4fed39fbad968e941517f06b2548a99829
kinoubenkyou <kinoubenkyou@gmail.com>
2023-03-23 16:30:34 +0700

    #80 Separate serializers by view

--- main/views/organization_personnel_view.py
+++ main/views/organization_personnel_view.py
@@ -1,4 +1,3 @@
-from django.db import transaction
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.mixins import (
@@ -13,8 +12,9 @@ from rest_framework.viewsets import GenericViewSet
 
 from main.models.personnel import Personnel
 from main.permissions.organization_permission import OrganizationPermission
-from main.permissions.user_permission import UserPermission
-from main.serializers.personnel_serializer import PersonnelSerializer
+from main.serializers.organization_personnel_serializer import (
+    OrganizationPersonnelSerializer,
+)
 
 
 class OrganizationPersonnelViewSet(
@@ -25,10 +25,10 @@ class OrganizationPersonnelViewSet(
     RetrieveModelMixin,
 ):
     filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ("does_organization_agree", "does_user_agree")
-    permission_classes = (OrganizationPermission, UserPermission)
+    filterset_fields = ("does_user_agree", "user")
+    permission_classes = (OrganizationPermission,)
     queryset = Personnel.objects.all()
-    serializer_class = PersonnelSerializer
+    serializer_class = OrganizationPersonnelSerializer
 
     def get_queryset(self):
         return (
@@ -36,15 +36,8 @@ class OrganizationPersonnelViewSet(
         )
 
     @action(detail=True, methods=("post",))
-    @transaction.atomic()
     def agreeing(self, request, *args, **kwargs):
         personnel = self.get_object()
         personnel.does_organization_agree = True
         personnel.save()
         return Response(status=HTTP_204_NO_CONTENT)
-
-    def create(self, request, *args, **kwargs):
-        request.data["does_organization_agree"] = True
-        request.data["does_user_agree"] = False
-        request.data["organization"] = kwargs["organization_id"]
-        return super().create(request, *args, **kwargs)
