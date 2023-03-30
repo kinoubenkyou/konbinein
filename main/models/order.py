15fded85dd444faae315826b78afdd552b4c5895
kinoubenkyou <kinoubenkyou@gmail.com>
2023-03-30 14:55:51 +0700

    #107 Add/change unique constraints for Staff, Order

--- main/models/order.py
+++ main/models/order.py
@@ -1,9 +1,25 @@
-from django.db.models import CASCADE, CharField, DateTimeField, ForeignKey, Model
+from django.db.models import (
+    CASCADE,
+    CharField,
+    DateTimeField,
+    ForeignKey,
+    Model,
+    UniqueConstraint,
+)
 
 from main.models.organization import Organization
 
 
 class Order(Model):
+    class Meta:
+        constraints = (
+            UniqueConstraint(
+                "code",
+                "organization",
+                name="main_order_code_organization_id",
+            ),
+        )
+
     code = CharField(max_length=255, unique=True)
     created_at = DateTimeField()
     organization = ForeignKey(Organization, on_delete=CASCADE)
