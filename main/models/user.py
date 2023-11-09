29ccd56553844f2c99a14cd80d20324a67118263
kinoubenkyou <kinoubenkyou@gmail.com>
2023-11-09 22:04:10 +0700

    #192 Fix missing migration

--- main/models/user.py
+++ main/models/user.py
@@ -4,8 +4,6 @@ from django.db.models import BooleanField, CharField, EmailField, Model
 class User(Model):
     authentication_token = CharField(max_length=255, null=True, unique=True)
     email = EmailField(unique=True)
-    email_verifying_token = CharField(max_length=255, null=True)
     hashed_password = CharField(max_length=255)
     is_system_administrator = BooleanField()
     name = CharField(max_length=255, null=True)
-    password_resetting_token = CharField(max_length=255, null=True)
